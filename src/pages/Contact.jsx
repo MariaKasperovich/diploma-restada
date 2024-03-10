92ca86f712a1c44b410e96f7bcd9692b601a82ab
MariaKasperovich <marikassp@gmail.com>
2024-03-10 12:28:20 +0300

    deloy bug fixes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Contact.css';
 import { ContactInfo } from '../components/ContactInfo';
-import { Form } from 'react-bootstrap';
+// import { Form } from 'react-bootstrap';
 import { Reviews } from '../components/Reviews';
 
 function Contact() {
